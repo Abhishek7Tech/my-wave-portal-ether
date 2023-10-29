00c9b74021be7bda4fa5ba88f3430f360d566cb7
Abhishek7Tech <abhishektab45@gmail.com>
2023-10-29 12:14:51 +0530

    build message form

--- wave-portal-frontend/src/App.js
+++ wave-portal-frontend/src/App.js
@@ -1,34 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import {Web3} from "web3";
-import {useEffect, useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Web3 } from "web3";
+import { useEffect, useState } from "react";
 import abi from "../src/utils/WavePortal.json";
 // import {ethers} from "ethers";
-const {ethers} = require("ethers");
+const { ethers } = require("ethers");
 
-const web3 = new Web3("https://proud-billowing-vineyard.ethereum-sepolia.discover.quiknode.pro/22c9e775bb68b11e36415cf7d5a135c9726e391f/");
+const web3 = new Web3(
+  "https://proud-billowing-vineyard.ethereum-sepolia.discover.quiknode.pro/22c9e775bb68b11e36415cf7d5a135c9726e391f/"
+);
 
 const getEthereumObject = () => window.ethereum;
 
 const findMetaMaskAccount = async () => {
   try {
-    
     const ethereum = getEthereumObject();
 
-    if(!ethereum) {
+    if (!ethereum) {
       console.log("Connect to metamask!");
       return;
     }
 
     console.log("GotCha Ethereum", ethereum);
 
-    const accounts = await ethereum.request({method: "eth_accounts"});
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    if(accounts.length !== 0) {
+    if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("ACCOUNT FOUND", account);
       return account;
-    }else {
+    } else {
       console.log("ACCOUNT NOT FOUND");
       return null;
     }
@@ -36,28 +37,74 @@ const findMetaMaskAccount = async () => {
     console.log("ERR", error);
     return null;
   }
-}
-
+};
 
 function App() {
-
   const [account, setAccount] = useState("");
+  const [waves, setWaves] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
+  const [message, setMessage] = useState("");
 
-  const contractAddress = "0x3978A079DDADC078DDa9C68Cc61A22254EAe60DA";
+  const contractAddress = "0xbD2297Cd2371C95A136372bc556011B8687faf87";
   const contractABI = abi.abi;
+
+  const messageHandler = (e) => {
+    e.preventDefault();
+    console.log("MESSAFG", e.target.value);
+
+    if (e.target.value.length) {
+      setMessage(e.target.value);
+    }
+  };
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+
+        const wavePortalContractStatic = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          provider
+        );
+
+        const waves = await wavePortalContractStatic.getAllWaves();
+
+        let wavesCleaned = [];
+
+        waves.forEach((wave) => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(Number(wave.timestamp) * 1000),
+            message: wave.message,
+          });
+        });
+
+        setAllWaves(wavesCleaned);
+        // let wavesCleaned = await waves.map((wave) => console.log("IN WAVE",wave));
+
+        console.log("WAVES", waves);
+      }
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
 
   const connectWallet = async () => {
     console.log("CLICKED");
     try {
       const ethereum = getEthereumObject();
 
-      if(!ethereum) {
+      if (!ethereum) {
         alert("Get Metamask");
         return;
       }
 
       const accounts = await ethereum.request({
-        method: "eth_requestAccounts"
+        method: "eth_requestAccounts",
       });
 
       console.log("Connected", accounts[0]);
@@ -65,34 +112,49 @@ function App() {
     } catch (error) {
       console.log("ERRORS", error);
     }
-  }
+  };
 
   useEffect(() => {
     findMetaMaskAccount().then((account) => {
-      if(account !== null) {
+      if (account !== null) {
         setAccount(account);
       }
-    })
-  },[]);
+    });
+    getAllWaves();
+  }, []);
 
-  const wave = async () => {
+  const wave = async (e) => {
+    e.preventDefault();
+
+    if (!message) {
+      alert("Write a message!");
+    }
     try {
-      const {ethereum} = window;
-      
-      if(ethereum) {
+      const { ethereum } = window;
+
+      if (ethereum) {
         const provider = new ethers.BrowserProvider(ethereum);
         const signer = await provider.getSigner();
 
         // Read data from the contract - Static
-        const wavePortalContractStatic = new ethers.Contract(contractAddress, contractABI, provider);
+        const wavePortalContractStatic = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          provider
+        );
 
         // Write data in the contract - Dynamic
-        const wavePortalContractDynamic = new ethers.Contract(contractAddress, contractABI, signer); 
-      // console.log("DEPLOYED", await wavePortalContract.getDeployedCode());
+        const wavePortalContractDynamic = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        // console.log("DEPLOYED", await wavePortalContract.getDeployedCode());
         let count = await wavePortalContractStatic.getTotalWaves();
         console.log("Total waves", count.toString());
-     
-        const waveTxn = await wavePortalContractDynamic.wave();
+        setWaves(count.toString());
+
+        const waveTxn = await wavePortalContractDynamic.wave(message);
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -101,38 +163,59 @@ function App() {
 
         count = await wavePortalContractStatic.getTotalWaves();
         console.log("Current Total waves", count.toString());
-     
-      }else {
+
+        setWaves(count.toString());
+        setMessage("");
+
+        e.target.message.value = "";
+      } else {
         console.log("Ethereum object not found!");
       }
     } catch (error) {
       console.log("ERROR", error);
     }
-    
-  }
+  };
   return (
     <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
 
-    <div className="dataContainer">
-      <div className="header">
-      👋 Hey there!
+        <div className="bio">
+          I am Abhishek and I am a web developer so that's pretty cool right?
+          Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <form className="waveForm" onSubmit={wave}>
+          <label >Message : 
+          <input className="inputMessage" onChange={messageHandler} name="message" type="text"></input>
+          </label>
+          <button type="submit" className="waveButton">
+            Wave at Me
+          </button>
+        </form>
+
+        {!account && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+        {waves && <h2 className="bio">Total Waves: {waves}</h2>}
+
+        {allWaves?.map((wave, index) => {
+          return (
+            <div
+              key={index}
+              className="message"
+            >
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>
+          );
+        })}
       </div>
-
-      <div className="bio">
-      I am Abhishek and I am a web developer so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-      </div>
-
-      <button className="waveButton" onClick={wave}>
-        Wave at Me
-      </button>
-
-      {
-        !account && (
-          <button className='waveButton' onClick={connectWallet}>Connect Wallet</button>
-        )
-      }
     </div>
-  </div>
   );
 }
 
